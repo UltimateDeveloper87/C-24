fc084f770f3c54b31cc30028acf1bf96877d8ec4
Ultimate Developer <73461885+UltimateDeveloper87@users.noreply.github.com>
2020-12-28 19:22:16 +0530

    Add files via upload

---
+++ pig.js
@@ -0,0 +1,34 @@
+class Pig {
+
+    //---PIG -REMOVED THE WIDTH AND HEIGHT BECAUSE KEEPING IT FIXED AT 50
+      constructor(x, y) {
+        var options = {
+            'restitution':0.8,
+            'friction':0.3,
+            'density':1.0
+        }
+  
+        //--WIDTH AND HEIGHT IS FIXED AT 50
+        this.body = Bodies.rectangle(x, y, 50, 50, options);
+        this.width = 50;
+        this.height = 50;
+        
+        
+        World.add(myWorld, this.body);
+      }
+      display(){
+        var pos =this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+       
+        //--ADDED IN C24- FILL
+        fill("green");
+  
+        rect(0, 0, this.width, this.height);
+        pop();
+      }
+    };
+    
